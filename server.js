4f05652759db7aa1d2d405a7d6f143c58e23f0a3
MartyV33 <martinverdoza33@gmail.com>
2024-09-13 21:05:10 -0500

    created a new.ejs file inside the views directory

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const path = require('path');
+require('dotenv').config();
+
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -14,6 +16,20 @@ app.use(methodOverride('_method'));
 app.get('/test', (req, res) => {
     res.send('Server is running properly!');
 });
+app.get('/foods/new', (req, res) => {
+    res.render('new');
+});
+
+app.post('/foods', async (req, res) => {
+    const newFood = new Food(req.body);
+    await newFood.save();
+    res.redirect('/foods');
+});
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
 
 const PORT = 1500;
 app.listen(PORT, () => {
