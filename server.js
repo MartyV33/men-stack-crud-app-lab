443704bd8e2ba4b621c7ad81db4a7a3fce8670b6
MartyV33 <martinverdoza33@gmail.com>
2024-09-13 19:46:38 -0500

    Added my foodSchema

---
+++ server.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const foodSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    calories: Number,
+    protien: Number,
+    carbs: Number,
+    fat: Number
+});
